32bea31455167af4dcf58ecf934ea8a14d9228a4
RAUriel <urielonefusion@gmail.com>
2024-08-21 01:03:14 -0600

    Ya se pueden observar los sensores que se usaran, falta la funcionalidad de la base de datos

--- navi/lib/config/presentations/screen/heartrate/heartrate_screen.dart
+++ navi/lib/config/presentations/screen/heartrate/heartrate_screen.dart
@@ -13,27 +13,54 @@ class HeartRateScreen extends StatefulWidget {
 class _HeartRateScreenState extends State<HeartRateScreen>
     with SingleTickerProviderStateMixin {
   double heartRate = 75.0; // Ritmo cardíaco simulado
+  double temperature = 36.5; // Temperatura simulada
+  double humidity = 50.0; // Humedad simulada
+
   late AnimationController _controller;
   late Animation<double> _animation;
+  late Animation<double> _temperatureAnimation;
+  late Animation<double> _humidityAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    // Configuración de la animación
+    // Configuración del controlador de animación
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    )..repeat(reverse: true); // Repite la animación en bucle
+      duration: const Duration(milliseconds: 2000),
+    )..repeat(reverse: true);
 
-    _animation = Tween<double>(begin: 1.0, end: 1.2).animate(
+    // Configuración de la animación para el ritmo cardíaco
+    _animation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Configuración de la animación para la temperatura
+    _temperatureAnimation = Tween<double>(begin: 0.0, end: 10.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Configuración de la animación para la humedad
+    _humidityAnimation = Tween<double>(begin: 0.0, end: 100.0).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
 
     MqttService.obtenerPulsosStream().listen((event) {
       setState(() {
-        // Actualizar el ritmo cardíaco
         heartRate = event;
+      });
+    });
+
+    MqttService.obtenerTemperaturaStream().listen((event) {
+      setState(() {
+        temperature = event;
+      });
+    });
+
+    MqttService.obtenerHumedadStream().listen((event) {
+      setState(() {
+        humidity = event;
       });
     });
   }
@@ -44,11 +71,15 @@ class _HeartRateScreenState extends State<HeartRateScreen>
     super.dispose();
   }
 
+  Color _getTemperatureColor(double temperature) {
+    return temperature > 30 ? const Color.fromARGB(255, 208, 187, 3) : Colors.blue;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ritmo Cardiaco'),
+        title: const Text('Ritmo Cardiaco, Temperatura y Humedad'),
         centerTitle: true,
         backgroundColor: Colors.teal,
       ),
@@ -56,12 +87,21 @@ class _HeartRateScreenState extends State<HeartRateScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text(
+              'Dashboard',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 20),
             // Corazón grande con efecto de latido
             ScaleTransition(
               scale: _animation,
               child: Container(
-                width: 150,
-                height: 150,
+                width: 120,
+                height: 120,
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.red,
@@ -80,13 +120,13 @@ class _HeartRateScreenState extends State<HeartRateScreen>
                   children: [
                     const Icon(
                       Icons.favorite,
-                      size: 50,
+                      size: 40,
                       color: Colors.white,
                     ),
                     Text(
                       '$heartRate BPM',
                       style: const TextStyle(
-                        fontSize: 24,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
@@ -95,16 +135,138 @@ class _HeartRateScreenState extends State<HeartRateScreen>
                 ),
               ),
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 20),
             const Text(
               'Ritmo Cardíaco',
               style: TextStyle(
-                fontSize: 24,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
                 color: Colors.teal,
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 20),
+            // Sensor de temperatura con color variable y efecto sutil
+            AnimatedBuilder(
+              animation: _temperatureAnimation,
+              builder: (context, child) {
+                return Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _getTemperatureColor(temperature),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: Transform.translate(
+                    offset: Offset(0, _temperatureAnimation.value),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.thermostat,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          '$temperature °C',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Temperatura',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Sensor de humedad con efecto de llenado
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.water_drop,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        '$humidity %',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Contenedor para el efecto de llenado
+                AnimatedBuilder(
+                  animation: _humidityAnimation,
+                  builder: (context, child) {
+                    return Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: 120,
+                        height: (120 * (_humidityAnimation.value / 100)),
+                        decoration: BoxDecoration(
+                          color: Colors.blue.withOpacity(0.5),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Humedad',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
+              ),
+            ),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context); // Regresar a la pantalla anterior

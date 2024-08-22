af1d3e1e340187e5b6b65a69a363566c1b175d3c
TaobT <ninjotase165@gmail.com>
2024-08-22 13:41:39 -0600

    sensores

--- navi/ESP32/ESP32.ino
+++ navi/ESP32/ESP32.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <PulseSensorPlayground.h>
+#include "DHT.h"
 
 // Configuración de WiFi
 const char* ssid = "Ubee28C4-2.4G";
@@ -9,7 +10,9 @@ const char* password = "96E6F328C4";
 // Configuración del broker MQTT
 const char* mqtt_server = "broker.emqx.io";
 const int mqtt_port = 1883;
-const char* mqtt_topic = "navi/beat";
+const char* mqtt_topic_beat = "navi/beat";
+const char* mqtt_topic_temp = "navi/temperature";
+const char* mqtt_topic_hum = "navi/humidity";
 
 // Configuración del sensor de pulso
 const int PulseWire = 34; 
@@ -18,12 +21,20 @@ const int PULSE_FADE = 5;
 const int THRESHOLD = 685;  
 PulseSensorPlayground pulseSensor;
 
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define BUZZER_PIN 14
+
 // Configuración de WiFi y MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(BUZZER_PIN, OUTPUT);
   
   // Conexión a WiFi
   setup_wifi();
@@ -40,6 +51,8 @@ void setup() {
   pulseSensor.setSerial(Serial);
   pulseSensor.begin();
   pulseSensor.setThreshold(THRESHOLD);
+
+  dht.begin();
 }
 
 void loop() {
@@ -52,15 +65,39 @@ void loop() {
   // Lectura del pulso
   int myBPM = pulseSensor.getBeatsPerMinute(); 
 
+
+  float humedad = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  if(!isnan(humedad)){
+    String humString = String(humedad);
+    client.publish(mqtt_topic_hum, humString.c_str());
+    Serial.print("Temp: ");
+    Serial.println(temperatura);
+  }
+
+  if(!isnan(temperatura)){
+    String tempString = String(temperatura);
+    client.publish(mqtt_topic_temp, tempString.c_str());
+    Serial.print("Hum: ");
+    Serial.println(humedad);
+  }
+
+
   // Enviar datos si se detecta un latido
   if (pulseSensor.sawStartOfBeat()) {
     Serial.println("Latido detectado!");
     Serial.print("BPM: ");
     Serial.println(myBPM);
+
+    if(myBPM > 100){
+      tone(BUZZER_PIN, 1000);
+    }
+    else noTone(BUZZER_PIN);
     
     // Publicar la señal en el broker MQTT
     String bpmString = String(myBPM);
-    client.publish(mqtt_topic, bpmString.c_str());
+    client.publish(mqtt_topic_beat, bpmString.c_str());
   }
 }
 
